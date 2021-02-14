2f2815a97721a80b0c8affcda6d6403f005062f5
iovidius <josjr2009@live.nl>
2021-02-15 00:32:01 +0100

    ignore .env

--- postbot_siemen.py
+++ postbot_siemen.py
@@ -1,4 +1,4 @@
-# Postbot Siemen is a Twitter based on Postbode Siemen (Zaai). He can:
+# Postbot Siemen is a Twitter bot based on Postbode Siemen (Zaai). He can:
 #   1) Give the translation of any word to the secret code of 'Wokkie Tokkie' 
 #   2) Tell people who are being bored that he has to work
 
