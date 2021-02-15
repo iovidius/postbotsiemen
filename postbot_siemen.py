772bc088a9e0dede53f1d350e7d4f656827b3e88
iovidius <josjr2009@live.nl>
2021-02-15 01:15:36 +0100

    from wokkie tokkie

--- postbot_siemen.py
+++ postbot_siemen.py
@@ -25,17 +25,37 @@ vowels = {
   'e': '2',
   'i': '3',
   'o': '4',
-  'u': '5'
+  'u': '5',
+  '1':'a',
+  '2':'e',
+  '3':'i',
+  '4':'o',
+  '5':'u'
 }
 
-def wokkie_tokkie(input):
-    # Returns the translation of a string to 'wokkie tokkie'
+# Returns the translation of an input string to 'wokkie tokkie'
+def to_wokkietokkie(input): 
     output = ''
     for c in input:
         if (c in vowels):
             output += vowels[c] + ' '
         else:
-            output += c + 'okkie '
+            output += c +  'okkie ' if c.isalpha() else c
     return output.strip()
 
-print(wokkie_tokkie('hut'))
+# Returns the translation of a 'wokkie tokkie' string to a normal string
+# Expects an input in wokkie tokkie format.
+def from_wokkietokkie(input):
+
+    # remove spaces
+    output = input.replace(' ','')
+
+    # replace all consonants
+    output = output.replace('okkie','')
+
+    # replace all numbers
+    for c in output:
+        if (c in vowels):
+            output = output.replace(c,vowels[c])
+
+    return output
